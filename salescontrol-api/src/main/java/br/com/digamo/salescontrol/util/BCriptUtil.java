1c33e4546cd7489bba6adf73949d207944805d69
digamo <digamo@gmail.com>
2020-10-08 10:43:46 -0300

    Criação do método boolean match(String encodedPassword, String passwd)

--- salescontrol-api/src/main/java/br/com/digamo/salescontrol/util/BCriptUtil.java
+++ salescontrol-api/src/main/java/br/com/digamo/salescontrol/util/BCriptUtil.java
@@ -5,6 +5,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class BCriptUtil {
 
+	/**
+	 * Encodes the password with SHA-1 hash encryption algorithm.
+	 * @param passwd
+	 * @return
+	 */
 	public static String encript(String passwd) {
 		
 		PasswordEncoder encoder = new BCryptPasswordEncoder();
@@ -12,5 +17,19 @@ public class BCriptUtil {
 		return encoder.encode(passwd);
 		
 	}
-	
+
+	/**
+	 * Returns true if passwords match, false if they don't match.
+	 * @param encodedPassword
+	 * @param passwd
+	 * @return
+	 */
+	public static boolean match(String encodedPassword, String passwd) {
+		
+		PasswordEncoder encoder = new BCryptPasswordEncoder();
+		
+		return encoder.matches(passwd, encodedPassword);
+		
+	}
+
 }

3ef438fe770e4c0a608752892a9cfde11c9b112c
digamo <digamo@gmail.com>
2020-10-12 21:15:14 -0300

    Inclusão das configurações do Swagger

---
+++ salescontrol-api/src/main/java/br/com/digamo/salescontrol/config/Swagger/SwaggerConfiguration.java
@@ -0,0 +1,41 @@
+package br.com.digamo.salescontrol.config.Swagger;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import br.com.digamo.salescontrol.model.entity.User;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("br.com.digamo.salescontrol"))
+                .paths(PathSelectors.ant("/**"))
+                .build()
+                .ignoredParameterTypes(User.class)
+                .globalOperationParameters(
+                        Arrays.asList(
+                                new ParameterBuilder()
+                                    .name("Authorization")
+                                    .description("Header para Token JWT")
+                                    .modelRef(new ModelRef("string"))
+                                    .parameterType("header")
+                                    .required(false)
+                                    .build()));
+    }
+
+
+}
